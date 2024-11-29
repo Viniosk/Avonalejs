a9be83067cf4482de9f9a7e4a5fee4c36e832666
Viniosk-15 <vinioslima124@gmail.com>
2024-11-29 14:09:51 -0300

    Atualizações Conect

--- src/http-common.js
+++ src/http-common.js
@@ -1,23 +1,23 @@
-import Axios from 'axios';
+// import Axios from 'axios';
 
-export const creatAxios = Axios.create({
-    baseURL: "https://localhost:7175"
-});
+// export const creatAxios = Axios.create({
+//     baseURL: "https://localhost:7175"
+// });
 
-const baseURL = "http://localhost:5000/";
+// const baseURL = "http://localhost:5000/";
 
-export const getTarefas = async () => {
-    const data = await Axios.get(baseURL +  "weatherforecast");
-    return data.data;
-}
+// export const getTarefas = async () => {
+//     const data = await Axios.get(baseURL +  "weatherforecast");
+//     return data.data;
+// }
 
 
-// Isso é no login
-export const postLogin = async (email, senha) => {
-    const data = await Axios.post(baseURL + "login", {
-        email: email,
-        senha: senha,
-    });
+// // Isso é no login
+// export const postLogin = async (email, senha) => {
+//     const data = await Axios.post(baseURL + "login", {
+//         email: email,
+//         senha: senha,
+//     });
 
-    return data.status;
-};
+//     return data.status;
+// };
